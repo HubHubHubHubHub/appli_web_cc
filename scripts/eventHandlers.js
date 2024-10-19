45d30a8ddf5b4cc87e57f38abcde36996bcd08bd
Netchaiev <captainbinouz@yahoo.fr>
2024-10-19 13:46:16 +0200

    ajout de la fonctionnalité pour faire apparaitre des variantes grammaticales à l'aide d'une checkbox

--- scripts/eventHandlers.js
+++ scripts/eventHandlers.js
@@ -14,4 +14,50 @@ let eventHandlers = {
       });
     }
   },
+  // Fonction pour attacher les événements des phrases d'exemple
+  setupExamplePhrasesEventHandlers: function () {
+    const wordDetailsContainer = document.getElementById("word-details");
+    if (wordDetailsContainer) {
+      wordDetailsContainer.addEventListener("change", function (event) {
+        const target = event.target;
+        if (target && target.classList.contains("show-verb-forms-checkbox")) {
+          const isChecked = target.checked;
+          const verbFormsContainerId = target.getAttribute("data-container");
+          const verbFormsContainer =
+            document.getElementById(verbFormsContainerId);
+          if (verbFormsContainer) {
+            if (isChecked) {
+              // Récupérer les données depuis les attributs data-*
+              const verbe = target.getAttribute("data-verbe");
+              const temps = target.getAttribute("data-temps");
+              const frag1 = target.getAttribute("data-frag1");
+              const frag2 = target.getAttribute("data-frag2");
+
+              // Générer les formes verbales
+              const verbFormsHtml = gramFunc.generateVerbForms(
+                verbe,
+                temps,
+                frag1,
+                frag2
+              );
+
+              // Insérer le contenu dans le conteneur
+              verbFormsContainer.innerHTML = verbFormsHtml;
+
+              // Afficher le conteneur
+              verbFormsContainer.style.display = "block";
+
+              // Appliquer les accents et réattacher les événements si nécessaire
+              accentManager.attachOriginalText();
+              accentManager.highlightWords();
+            } else {
+              // Cacher le conteneur et effacer son contenu
+              verbFormsContainer.style.display = "none";
+              verbFormsContainer.innerHTML = "";
+            }
+          }
+        }
+      });
+    }
+  },
 };
