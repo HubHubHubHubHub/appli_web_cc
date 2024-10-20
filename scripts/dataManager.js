fd42349ad26b7b4696509f431eecc8d62c35923f
Netchaiev <captainbinouz@yahoo.fr>
2024-10-20 13:31:25 +0200

    chargements de la nouvelle base de donnée phrases.json à l'aide de async

--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -1,18 +1,34 @@
-// Chargement des données JSON
-dataManager = {
-  wordData: {},
+// dataManager.js
 
-  loadWordData: function (callback) {
-    fetch("../data.json")
-      .then((response) => response.json())
-      .then((json) => {
-        dataManager.wordData = json;
-        if (typeof callback === "function") {
-          callback();
+const dataManager = {
+  wordData: {},
+  phraseData: {},
+
+  loadWordData: function () {
+    return fetch("../data.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors du chargement de data.json");
         }
+        return response.json();
       })
-      .catch((error) =>
-        console.error("Erreur lors du chargement du fichier JSON:", error)
-      );
+      .then((json) => {
+        this.wordData = json;
+        return json;
+      });
+  },
+
+  loadPhrasesData: function () {
+    return fetch("../phrases.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors du chargement de phrases.json");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.phraseData = json;
+        return json;
+      });
   },
 };
