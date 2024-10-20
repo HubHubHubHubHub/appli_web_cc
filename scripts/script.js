cc12f396c081ab00bc9cf33e99d43e18d3e90008
Netchaiev <captainbinouz@yahoo.fr>
2024-10-20 13:31:25 +0200

    chargements de la nouvelle base de donnée phrases.json à l'aide de async

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Charge le fichier JSON
-  dataManager.loadWordData(() => {
+// script.js
+
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    // Attend le chargement de data.json
+    await dataManager.loadWordData();
+
+    // Attend le chargement de phrases.json
+    await dataManager.loadPhrasesData();
+
+    // Exécute les initialisations
     displayManager.displayWordList(); // Affiche la liste des mots
     accentManager.setupAccentHighlighting(); // Initialise la fonctionnalité après le chargement des données
     eventHandlers.setupWordListClickEvents(); // Attache les événements de clic sur les éléments de la liste
-  });
+  } catch (error) {
+    console.error("Erreur lors du chargement des fichiers JSON:", error);
+  }
 });
